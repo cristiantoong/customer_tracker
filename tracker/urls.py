2bbf4f5d0d5f012516a0c51503cb8a71a3fa0356
Cristian To-ong <me@gmail.com>
2019-07-21 15:27:17 +0800

    Import Export Function Added

--- tracker/urls.py
+++ tracker/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('<int:item_id>/', views.item_detail, name='item_detail'),
     path('<int:item_id>/update/', views.update_item, name='update_item'),
     path('<int:item_id>/delete/', views.delete_item, name='delete_item'),
+    path('import_export/', views.import_export, name='import_export'),
+    path('export/xls/', views.export_customer_xls, name='export_customer_xls'),
 
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Customer
 from .forms import CustomerForm
 from django.contrib import messages
+import xlwt
 
 
 @login_required
@@ -52,6 +54,7 @@ def update_item(request, item_id):
     return render(request, 'tracker/item_update.html', context)
 
 
+@login_required
 def delete_item(request, item_id):
     customer = get_object_or_404(Customer, pk=item_id)
     if request.method == 'POST':
@@ -62,5 +65,39 @@ def delete_item(request, item_id):
         'customer': customer
     }
     return render(request, 'tracker/item_delete.html', context)
-    # context = {'customer': customer}
-    # return render(request, 'tracker/home.html', context)
+
+
+def export_customer_xls(request):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename=".xls"'
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Customer')
+
+    # Sheet header, first row
+    row_num = 0
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    columns = ['Customer', 'Address', 'Product', 'Price']
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    # Sheet body, remaining rows
+    font_style = xlwt.XFStyle()
+
+    rows = Customer.objects.all().values_list('customer_name', 'address',
+                                              'product', 'price')
+    for row in rows:
+        row_num += 1
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, row[col_num], font_style)
+
+    wb.save(response)
+    return response
+
+
+def import_export(request):
+    return render(request, 'tracker/import_export.html')
